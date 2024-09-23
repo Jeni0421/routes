118ac2198a3d97c29ac72677a68d757fb3c67132
Jeni0421 <zauldajenilyn@gmail.com>
2024-09-23 19:16:13 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const PORT =3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes/router");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.use('/', routes);
+
+app.listen(3000, () => {
+    console.log(`server initialized on http://localhost:3000`);
+
+})
